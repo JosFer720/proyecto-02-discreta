f486f81317446e789d66b5c96f2b7495332fe1e9
erickguerra2 <eagi578@gmail.com>
2024-11-18 19:35:08 -0600

    Funcion desencriptar

--- Proyecto02.py
+++ Proyecto02.py
@@ -2,7 +2,7 @@
 # MATEMATICA DISCRETA 1 - 10
 # FERNANDO RUIZ - 23065
 # FABIAN MORALES - 23267
-# ERICK GUERRA - 23---
+# ERICK GUERRA - 23208
 
 import random
 
@@ -66,85 +66,98 @@ def generar_llaves(rango_inferior, rango_superior):
 
 
 def encriptar(caracter, llave_publica):
+    print(llave_publica)
     c = ((caracter)**llave_publica[0]) % llave_publica[1]
 
     return c
 
+def desencriptar(cifrado, llave_privada):
+    print(llave_privada)
+    m = ((cifrado)**llave_privada[0]) % llave_privada[1]
+
+    return m
+
 def main():
-    while True:
-        print("\nElige una opción:")
-        print("1. Generar un número primo al azar dentro de un rango")
-        print("2. Calcular el MCD de dos números")
-        print("3. Calcular el inverso modular de dos números")
-        print("4. Cifrado")
-        print("5. Adios")
-
-        opcion = input("Opción: ")
-
-        if opcion == "1":
-            try:
-                inferior = int(input("Ingresa el límite inferior del rango: "))
-                superior = int(input("Ingresa el límite superior del rango: "))
-                primo = generar_primo_azar(inferior, superior)
-                print(f"Número primo generado: {primo}")
-            except ValueError as e:
-                print(f"Error: {e}")
-
-        elif opcion == "2":
-            try:
-                a = int(input("Ingresa el primer número: "))
-                b = int(input("Ingresa el segundo número: "))
-                resultado_mcd = mcd(a, b)
-                print(f"El MCD de {a} y {b} es: {resultado_mcd}")
-            except ValueError as e:
-                print(f"Error: {e}")
-
-        elif opcion == "3":
-            try:
-                a = int(input("Ingresa el número a: "))
-                m = int(input("Ingresa el módulo m: "))
-                inverso = inverso_modular(a, m)
-                if inverso is not None:
-                    print(f"El inverso modular de {a} mod {m} es: {inverso}")
-                else:
-                    print(f"No existe inverso modular para {a} mod {m}.")
-            except ValueError as e:
-                print(f"Error: {e}")
-
-        elif opcion == "4":
-
-            numero_encriptar = int(input("Ingrese el número a encriptar: "))
-
-            try:
-
-                llaves = generar_llaves(11,1000)
-
-                if llaves is None:
-                    raise ValueError("Los primos generados no son válidos. Vuelva a intentarlo.")
-
-                else:
-                    print(llaves)
+    rango_inferior = int(input("Ingrese el rango inferior: "))
+    rango_superior = int(input("Ingrese el rango superior: "))
+    llave_publica, llave_privada = generar_llaves(rango_inferior,rango_superior)
+    print (llave_publica, llave_privada)
+    caracter = int(input("Ingrese un numero para encriptar: "))
+    cifrado = encriptar(caracter, llave_publica)
+    print(cifrado)
+    mensaje_descifrado = desencriptar(cifrado, llave_privada)
+    print(mensaje_descifrado)
 
 
-                    print(encriptar(numero_encriptar,llaves[0]))
+# def main():
+#     while True:
+#         print("\nElige una opción:")
+#         print("1. Generar un número primo al azar dentro de un rango")
+#         print("2. Calcular el MCD de dos números")
+#         print("3. Calcular el inverso modular de dos números")
+#         print("4. Cifrado")
+#         print("5. Descifrado")
+#         print("6. Adios")
 
-            except ValueError as e:
-                print(f"Se detectó un error: {e}")
+#         opcion = input("Opción: ")
+
+#         if opcion == "1":
+#             try:
+#                 inferior = int(input("Ingresa el límite inferior del rango: "))
+#                 superior = int(input("Ingresa el límite superior del rango: "))
+#                 primo = generar_primo_azar(inferior, superior)
+#                 print(f"Número primo generado: {primo}")
+#             except ValueError as e:
+#                 print(f"Error: {e}")
+
+#         elif opcion == "2":
+#             try:
+#                 a = int(input("Ingresa el primer número: "))
+#                 b = int(input("Ingresa el segundo número: "))
+#                 resultado_mcd = mcd(a, b)
+#                 print(f"El MCD de {a} y {b} es: {resultado_mcd}")
+#             except ValueError as e:
+#                 print(f"Error: {e}")
+
+#         elif opcion == "3":
+#             try:
+#                 a = int(input("Ingresa el número a: "))
+#                 m = int(input("Ingresa el módulo m: "))
+#                 inverso = inverso_modular(a, m)
+#                 if inverso is not None:
+#                     print(f"El inverso modular de {a} mod {m} es: {inverso}")
+#                 else:
+#                     print(f"No existe inverso modular para {a} mod {m}.")
+#             except ValueError as e:
+#                 print(f"Error: {e}")
+
+#         elif opcion == "4":
+
+#             numero_encriptar = int(input("Ingrese el número a encriptar: "))
+
+#             try:
+
+#                 llaves = generar_llaves(11,1000)
+
+#                 if llaves is None:
+#                     raise ValueError("Los primos generados no son válidos. Vuelva a intentarlo.")
+
+#                 else:
+#                     print(llaves)
 
 
+#                     print(encriptar(numero_encriptar,llaves[0]))
+
+#             except ValueError as e:
+#                 print(f"Se detectó un error: {e}")
+#         elif opcion == "5":
 
 
-            
+#         elif opcion == "6":
+#             print("Saliendo del programa.")
+#             break
 
-
-
-
-
-        elif opcion == "5":
-            print("Saliendo del programa.")
-            break
-
-        else:
-            print("Opción no válida, intenta de nuevo.")
+#         else:
+#             print("Opción no válida, intenta de nuevo.")
 
 main()
